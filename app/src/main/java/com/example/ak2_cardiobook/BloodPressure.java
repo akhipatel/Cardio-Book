41dcd7e142fdbb1bb5862294f3ecaf49ed806ff6
akhipatel <ak2@ualberta.ca>
2020-02-02 01:47:48 -0700

    Update

--- app/src/main/java/com/example/ak2_cardiobook/BloodPressure.java
+++ app/src/main/java/com/example/ak2_cardiobook/BloodPressure.java
@@ -1,7 +1,9 @@
+
 package com.example.ak2_cardiobook;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
 
 //TODO: Need to make another file that has a records array which will keep track of everything.
 
@@ -42,22 +44,86 @@ public class BloodPressure {
 
     }
 
-    /* Accessor */
+    /* Accessors Getters and Setters */
+    /**
+     * @return the daet
+     */
     public Date getDate() {
         return this.date;
     }
-
+    /**
+     * @param date the date to set
+     */
     public void setDate() {
         this.date = new Date();
     }
-
+    /**
+     * @return the time
+     */
     public String getTime() {
         return this.time;
     }
-
+    /**
+     * @param time the time to set
+     */
     public void setTime(Date date) {
         SimpleDateFormat sdf = new SimpleDateFormat("hh:mm");
         this.time = sdf.format(date);
+    }
+
+    /**
+     * @return the systolicPressure
+     */
+    public int getSystolicPressure() {
+        return systolicPressure;
+    }
+
+    /**
+     * @param systolicPressure the systolicPressure to set
+     */
+    public void setSystolicPressure(int systolicPressure) {
+        this.systolicPressure = systolicPressure;
+    }
+    /**
+     * @return the diastolicPressure
+     */
+    public int getDiastolicPressure() {
+        return diastolicPressure;
+    }
+
+    /**
+     * @param diastolicPressure the diastolicPressure to set
+     */
+    public void setDiastolicPressure(int diastolicPressure) {
+        this.diastolicPressure = diastolicPressure;
+    }
+    /**
+     * @return the heartRate
+     */
+    public int getHeartRate() {
+        return heartRate;
+    }
+    /**
+     * @param heartRate the heartRate to set
+     */
+    public void setHeartRate(int heartRate) {
+        this.heartRate = heartRate;
+    }
+    /**
+     * @return the comments
+     */
+    public String getComments() {
+        return comments;
+    }
+    /**
+     * @param comments the comments to set
+     */
+    public void setComments(String comments) {
+        if (comments.length() > 20){
+            System.out.println("Please input comment under 20 characters");
+        }else{
+            this.comments = comments;
+        }
     }
 
 }
